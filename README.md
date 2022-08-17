ead9210ba7e94f82c8a5a5a7908ded9fd89fcb61
Erik Pastuszek <erikpastuszek@gmail.com>
2022-08-16 23:16:06 -0300

    added routes

--- README.md
+++ README.md
@@ -169,17 +169,17 @@ Se debe desarrollar un servidor en Node/Express con las siguientes rutas:
 
 **IMPORTANTE**: No está permitido utilizar los filtrados, ordenamientos y paginados brindados por la API externa, todas estas funcionalidades tienen que implementarlas ustedes.
 
-- [ ] **GET /dogs**:
+- [✅] **GET /dogs**:
   - Obtener un listado de las razas de perro
   - Debe devolver solo los datos necesarios para la ruta principal
-- [ ] **GET /dogs?name="..."**:
+- [✅] **GET /dogs?name="..."**:
   - Obtener un listado de las razas de perro que contengan la palabra ingresada como query parameter
   - Si no existe ninguna raza de perro mostrar un mensaje adecuado
-- [ ] **GET /dogs/{idRaza}**:
+- [✅] **GET /dogs/{idRaza}**:
   - Obtener el detalle de una raza de perro en particular
   - Debe traer solo los datos pedidos en la ruta de detalle de raza de perro
   - Incluir los temperamentos asociados
-- [ ] **POST /dogs**:
+- [✅] **POST /dogs**:
   - Recibe los datos recolectados desde el formulario controlado de la ruta de creación de raza de perro por body
   - Crea una raza de perro en la base de datos relacionada con sus temperamentos
 - [ ] **GET /temperaments**:
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,38 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+//----------------DOGS-------------------
+//GET CON QUERY
+router.get('/dogs', async (req, res) => {
+	let resultado = 'peticion a la api y a la db';
+	const { query } = req.query;
+	if (query)
+		if (Object.keys(query).length > 0)
+			resultado = 'peticion a la api y a la db con query';
+	//ACA HAGO EL LLAMADO A TODOS LOS PERROS, A LOS DE LA API Y A LOS DE LA DB
+
+	res.json(resultado.data);
+});
+//GET CON PARAMS
+router.get('/dogs/:id', (req, res) => {
+	let resultado = 'peticion a la api y a la db con ID particular';
+	const { params } = req.params;
+	//ACA HAGO EL LLAMADO A TODOS LOS PERROS, A LOS DE LA API Y A LOS DE LA DB
+	res.json(resultado);
+});
+//POST PERROS
+router.post('/dogs', (req, res) => {
+	const { name, height, weight, lifespan } = req.body;
+	//ACA HAGO EL LLAMADO A TODOS LOS PERROS, A LOS DE LA API Y A LOS DE LA DB
+	let resultado = 'post a la DB de un perro';
+	res.json(resultado);
+});
+//<-------------- TENES QUE BUSCAR EN SEQUELIZE COMO HACER LA PETICION A LA DB ------------->
+//----------------TEMPERAMENTS------------------
 
 module.exports = router;
